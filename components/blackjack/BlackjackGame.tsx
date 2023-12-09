ad06d0a5f53beac46d4d2f0836a3ed04ff335de7
loudsheep <loudsheep05@gmail.com>
2023-12-09 16:45:18 +0100

    Small frontend changes

--- components/blackjack/BlackjackGame.tsx
+++ components/blackjack/BlackjackGame.tsx
@@ -17,86 +17,93 @@ type BlackjacjGameProps = {
 export default function BlackjackGame(props: BlackjacjGameProps) {
     const [showSpinner, setShowSpinner] = useState<boolean>(false);
 
-    const socket = io("http://localhost:3001");
+    // const socket = io("http://localhost:3001");
 
-    const joinRoom = () => {
-        let data: SocketIncomingData = {
-            roomId: props.roomId,
-            hash: props.gameHash,
-            token: props.token,
-            username: props.username,
-        };
+    // const joinRoom = () => {
+    //     let data: SocketIncomingData = {
+    //         roomId: props.roomId,
+    //         hash: props.gameHash,
+    //         token: props.token,
+    //         username: props.username,
+    //     };
 
-        socket.emit('join_room', data);
-    };
+    //     socket.emit('join_room', data);
+    // };
 
-    useEffect(() => {
-        // joinRoom();
-    }, []);
+    // useEffect(() => {
+    //     // joinRoom();
+    // }, []);
 
     return (
         <>
-        <body>
-            <div className="header">
-                <div>Game</div>
-                <div>Shop</div>
-                <div>Rules</div>
-                <div>Profile</div>
-            </div>
-            <div className="table">
-                <div className="dealer">
-                    <div>
-                    </div>
-                    <div><p>Dealer</p><p>cards</p>
-                        <div className="cards">
-                            <div className="card"></div>
+            <body>
+                <div className="header">
+                    <div>Game</div>
+                    <div>Shop</div>
+                    <div>Rules</div>
+                    <div>Profile</div>
+                </div>
+                <div className="table">
+                    <div className="dealer">
+                        <div>
+                        </div>
+                        <div><p>Dealer</p><p>cards</p>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                        <div className="deck">
                         </div>
                     </div>
-                    <div className="deck">
+                    <div className="players">
+                        <div>
+                            <div><p>Player 1</p><p>cards</p></div>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <div><p>Player 2</p><p>cards</p></div>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <div><p>Player 3</p><p>cards</p></div>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <div><p>Player 4</p><p>cards</p></div>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                        <div>
+                            <div><p>Player 5</p><p>cards</p></div>
+                            <div className="cards">
+                                <div className="card"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* taki ciekawy button znalazłem */}
+                    <button className="bg-green-950 text-green-400 border border-green-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                        <span className="bg-green-400 shadow-green-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                        Hover Me
+                    </button>
+
+                    <div className="action_buttons_space">
+                        <button>DOUBLE</button>
+                        <button>SPLIT</button>
+                        <button>STAND</button>
+                        <button>HIT</button>
                     </div>
                 </div>
-                <div className="players">
-                    <div>
-                        <div><p>Player 1</p><p>cards</p></div>
-                        <div className="cards">
-                            <div className="card"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <div><p>Player 2</p><p>cards</p></div>
-                        <div className="cards">
-                            <div className="card"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <div><p>Player 3</p><p>cards</p></div>
-                        <div className="cards">
-                            <div className="card"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <div><p>Player 4</p><p>cards</p></div>
-                        <div className="cards">
-                            <div className="card"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <div><p>Player 5</p><p>cards</p></div>
-                        <div className="cards">
-                            <div className="card"></div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="action_buttons_space">
-                    <button>DOUBLE</button>
-                    <button>SPLIT</button>
-                    <button>STAND</button>
-                    <button>HIT</button>
-                </div>
-            </div>
-            
-        </body>
+
+            </body>
         </>
     );
 }
