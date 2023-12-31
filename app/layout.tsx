bdac2c2d95ed951e77ce10df1290448b1c8c702e
loudsheep <loudsheep05@gmail.com>
2023-12-31 10:57:01 +0100

    Fixed og meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,22 @@ import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata: Metadata = {
-//     title: 'Blackjack!',
-//     description: 'Online Blackjack games for you and your friends!',
-// }
+export const metadata: Metadata = {
+    title: 'Blackjack!',
+    description: 'Online Blackjack games for you and your friends!',
+    openGraph: {
+        title: 'Blackjack!',
+        description: 'Online Blackjack games for you and your friends!',
+        images: process.env.BASE_PATH + "/link_image.jpg",
+        url: process.env.BASE_PATH
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Blackjack!',
+        description: 'Online Blackjack games for you and your friends!',
+        images: process.env.BASE_PATH + "/link_image.jpg",
+    }
+}
 
 export default function RootLayout({
     children,
@@ -17,15 +29,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <Head>
-                <title>Blackjack!</title>
-                <meta name="description">Online Blackjack games for you and your friends!</meta>
-                <meta property='og:title' content='Blackjack!' />
-                <meta property='og:description' content='Online Blackjack games for you and your friends!' />
-                <meta property='og:image' content={process.env.NEXT_PUBLIC_BASE_URL + "/link_image.jpg"} />
-                <meta property='og:url' content={process.env.NEXT_PUBLIC_BASE_URL} />
-            </Head>
-
             <body className={inter.className}>{children}</body>
         </html>
     )
