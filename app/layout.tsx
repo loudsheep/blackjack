33d0bd2fca5260d9f8e461d24ee12e8215f7bcbe
loudsheep <loudsheep05@gmail.com>
2023-12-31 10:35:59 +0100

    Added link preview meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,32 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Blackjack!',
-  description: 'Online Blackjack games for you and your friends!',
-}
+// export const metadata: Metadata = {
+//     title: 'Blackjack!',
+//     description: 'Online Blackjack games for you and your friends!',
+// }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Head>
+                <title>Blackjack!</title>
+                <meta name="description">Online Blackjack games for you and your friends!</meta>
+                <meta property='og:title' content='Blackjack!' />
+                <meta property='og:description' content='Online Blackjack games for you and your friends!' />
+                <meta property='og:image' content={process.env.NEXT_PUBLIC_BASE_URL + "/link_image.jpg"} />
+                <meta property='og:url' content={process.env.NEXT_PUBLIC_BASE_URL} />
+            </Head>
+
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
